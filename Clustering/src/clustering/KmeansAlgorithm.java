f2fcce6e732a0177d155a16e8e216d884a7086cf
frank <frank.rb997@gmail.com>
2018-11-13 18:58:34 +0100

    arreglo de las distancias entre iteraciones otra puta vez

--- Clustering/src/clustering/KmeansAlgorithm.java
+++ Clustering/src/clustering/KmeansAlgorithm.java
@@ -6,22 +6,20 @@ import java.util.Iterator;
 public class KmeansAlgorithm {
 	// número de clusters
 	private final int					k;
-	
-	//completelink o singlelink-->singlelink
+
+	// completelink o singlelink-->singlelink
 	private final String				distInter;
-	
-	//aleatorio o division o 2kclusters-->aleatorio
+
+	// aleatorio o division o 2kclusters-->aleatorio
 	private final String				inicializacion;
 	private final int					iteraciones;
 	private final double				umbral;
 	private final ArrayList<Cluster>	resultado;
 	private final String				tipoDistancia;
 
-	
-	
 	/**
 	 * Constructora para la clase
-	 * 
+	 *
 	 * @param pK
 	 * @param pDist
 	 *            //singlelink o completelink
@@ -30,8 +28,8 @@ public class KmeansAlgorithm {
 	 * @param pUmbr
 	 */
 	public KmeansAlgorithm(int pK, String pDist, String pInic, int pIter, double pUmbr, String pTipoDist) {
-		
-		//Inicializaciones
+
+		// Inicializaciones
 		k = pK;
 		distInter = pDist;
 		inicializacion = pInic;
@@ -40,81 +38,31 @@ public class KmeansAlgorithm {
 		umbral = pUmbr;
 		tipoDistancia = pTipoDist;
 	}
-	
-	
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////               MÉTODOS                          /////////////////////////////////////////////////
-///////////////////////////////              PARA CALCULAR                     /////////////////////////////////////////////////
-///////////////////////////////             EL K-MEANS                        //////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	/**
-	 * Método principal para calcular el K-Means Clustering
-	 */
-	public void calcularKmeans() {
-		
-		//Inicializaciones de variables y asignaciones de clusters
-		DataBase.getDataBase().inicializarInstancias();
-		asignarVectorClusters();
-		asignarInstanciasClusters();
-		int i = 0;
-		
-		//Imprimimos por pantalla los valores iniciales seleccionados
-		System.out.println("**¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*");
-		System.out.println("**¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*");
-		System.out.println("Valores iniciales clusters");
-		System.out.println("**¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*");
-		System.out.println("**¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*");
-		imprimirClusters();
-		
-		while (i < iteraciones) {
-			
-			ArrayList<String> list1 = this.resultado.get(0).getCentroide();
-			recalcularCentroides();
-			asignarInstanciasClusters();
-			double dist = this.resultado.get(0).calcularDistancia(list1, tipoDistancia);
-			System.out.println("Distancia iteración No" + i + ":  " + dist);
-			
-			if (dist <= umbral) {
-				break;
-			}
-			i++;
-		}
-		
-		//Imprimimos los datos de los Clusters
-		System.out.println("***********************************");
-		System.out.println("***********************************");
-		System.out.println("Instancias totales de la muestra: " + DataBase.getDataBase().getInstancias().size());
-		imprimirClusters();
-	}
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////// MÉTODOS
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// /////////////////////////////////////////////////
+	/////////////////////////////// PARA CALCULAR
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// /////////////////////////////////////////////////
+	/////////////////////////////// EL K-MEANS
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// //////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	
-	/**
-	 * Recalcula los centroides para la siguiente iteración.
-	 */
-	private void recalcularCentroides() {
-		Iterator<Cluster> it = resultado.iterator();
-		while (it.hasNext()) {
-			it.next().recalcularCentroide();
-		}
-
-	}
-	
-	
 	/**
 	 * Asigna a una instancia el Clúster que esté más cerca
+	 *
 	 * @param pInst
 	 */
 	public void asignarInstancia(Instancia pInst) {
-		
-		//Inicializaciones del método
+
+		// Inicializaciones del método
 		int i = 0;
 		int pos = 0;
 		double distancia = this.resultado.get(0).calcularDistancia(pInst.getLista(), tipoDistancia);
 		Iterator<Cluster> it = this.resultado.iterator();
-		
+
 		while (it.hasNext()) {
 			Cluster nuevo = it.next();
 			if (nuevo.calcularDistancia(pInst.getLista(), tipoDistancia) < distancia) {
@@ -125,15 +73,14 @@ public class KmeansAlgorithm {
 		this.resultado.get(pos).addInstancia(pInst);
 	}
 
-	
 	/**
 	 * Asigna todas las instancias a sus correspondientes Clústers más cercanos
-	 * 
+	 *
 	 */
 	public void asignarInstanciasClusters() {
 
 		Iterator<Instancia> it = DataBase.getDataBase().getInstancias().iterator();
-		
+
 		while (it.hasNext()) {
 			Instancia inst = it.next();
 			asignarInstancia(inst);
@@ -141,16 +88,19 @@ public class KmeansAlgorithm {
 	}
 
 	/**
-	 * Imprime los datos de los clusters por pantalla
+	 * Asigna vectores iniciales en la variación 2k del K-Means
 	 */
-	private void imprimirClusters(){
-		
-		Iterator<Cluster> it = resultado.iterator();
-		Cluster act;
-		while(it.hasNext()){
-			act = it.next();
-			act.printCentroide();
-			act.printCluster();
+	public void asignarVector2kClusters() {
+		// TODO Auto-generated method stub
+		int i = 0;
+		System.out.println("Valores de los centroides iniciales: ");
+		while (i < 2 * k) {
+			Cluster nuevo = new Cluster(getVectorAleatorio());
+			if (!resultado.contains(nuevo)) {
+				resultado.add(nuevo);
+				nuevo.printCentroide();
+				i++;
+			}
 		}
 	}
 
@@ -158,34 +108,23 @@ public class KmeansAlgorithm {
 	 * Inicializa los centroides iniciales
 	 */
 	public void asignarVectorAleatorioClusters() {
-		
+
 		int i = 0;
 		System.out.println("Valores de los centroides iniciales: ");
-		
+
 		while (i < k) {
-			
+
 			Cluster nuevo = new Cluster(getVectorAleatorio());
-			
+
 			if (!resultado.contains(nuevo)) {
-				
+
 				resultado.add(nuevo);
 				nuevo.printCentroide();
 				i++;
 			}
 		}
 	}
-	
-	
-	/**
-	 * Método para obtener un Vector Aleatorio
-	 * @return
-	 */
-	public Instancia getVectorAleatorio() {
 
-		return DataBase.getDataBase().getRandomVector();
-	}
-	
-	
 	/**
 	 * Asignamos un vector a un Cluster dependiendo del tipo de inicialización
 	 */
@@ -204,6 +143,55 @@ public class KmeansAlgorithm {
 			System.out.println("Error al asignar clusters");
 			break;
 		}
+	}
+
+	/**
+	 * Método principal para calcular el K-Means Clustering
+	 */
+	public void calcularKmeans() {
+
+		// Inicializaciones de variables y asignaciones de clusters
+		DataBase.getDataBase().inicializarInstancias();
+		asignarVectorClusters();
+		asignarInstanciasClusters();
+		int i = 0;
+
+		// Imprimimos por pantalla los valores iniciales seleccionados
+		System.out.println("**¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*");
+		System.out.println("**¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*");
+		System.out.println("Valores iniciales clusters");
+		System.out.println("**¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*");
+		System.out.println("**¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*¿*");
+		imprimirClusters();
+
+		while (i < iteraciones) {
+			int j = 0;
+			Centroides oldCentroides = new Centroides();
+			while (j < this.resultado.size()) {
+				oldCentroides.add(this.resultado.get(j).getCentroide());
+				j++;
+			}
+			recalcularCentroides();
+			asignarInstanciasClusters();
+			double dist = 0;
+			int z = 0;
+			while (z < this.resultado.size()) {
+				dist = dist + this.resultado.get(z).calcularDistancia(oldCentroides.get(z), tipoDistancia);
+				z++;
+			}
+			dist = dist / resultado.size();
+			System.out.println("Distancia iteración No" + i + ":  " + dist);
+			if (dist <= umbral) {
+				break;
+			}
+			i++;
+		}
+
+		// Imprimimos los datos de los Clusters
+		System.out.println("***********************************");
+		System.out.println("***********************************");
+		System.out.println("Instancias totales de la muestra: " + DataBase.getDataBase().getInstancias().size());
+		imprimirClusters();
 	}
 
 	public void compararClustersInstancias() {
@@ -231,55 +219,46 @@ public class KmeansAlgorithm {
 		/////////////////////////////////////////////////////////////////
 		return null;
 	}
-	
-	
-	
-	
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////               MÉTODOS                          /////////////////////////////////////////////////
-//////////////////////////////              PARA CALCULAR                      /////////////////////////////////////////////////
-/////////////////////////////               EL SILHOUETTE                 /////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	
 	/**
 	 * Método para calcular el Silhouette
+	 *
 	 * @param pInstancia
 	 * @param pCluster
 	 * @return double
 	 */
-	
+
 	public double getSilhouette(Instancia pInstancia, Cluster pCluster) {
-		
+
 		double Silhouette = 0;
 		// Calculamos la cohesión
 		double cohexion = pCluster.getDistanciaMedia(pInstancia, tipoDistancia);
-		
+
 		// Calculamos la separación
 		Cluster masCercano = getClusterMasCercano(pCluster);
 		double separacion = masCercano.getDistanciaMedia(pInstancia, tipoDistancia);
-		
-		if (cohexion >= separacion)
+
+		if (cohexion >= separacion) {
 			Silhouette = (separacion - cohexion) / cohexion;
-		else
+		} else {
 			Silhouette = (separacion - cohexion) / separacion;
-		if (Double.isNaN(Silhouette))
-			Silhouette = 0.0;	
+		}
+		if (Double.isNaN(Silhouette)) {
+			Silhouette = 0.0;
+		}
 		// Comparamos la cohesión y la separación
 		return Silhouette;
 	}
-	
+
 	/**
 	 * Método principal para realizar el Silhouette
+	 *
 	 * @return double
 	 */
-	
-	
+
 	public double getSilhouetteAgrupamiento() {
-		
-		//Inicializaciones del método
+
+		// Inicializaciones del método
 		double shilhouette = 0;
 		int i = 0;
 		Iterator<Cluster> it = resultado.iterator();
@@ -307,58 +286,36 @@ public class KmeansAlgorithm {
 
 		return shilhouette / i;
 	}
-	
-	
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////// MÉTODOS
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// /////////////////////////////////////////////////
+	////////////////////////////// PARA CALCULAR
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// /////////////////////////////////////////////////
+	///////////////////////////// EL SILHOUETTE
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// /////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	/**
-	 * Halla el clúster más cercano a pCluster
-	 * @param pCluster
+	 * Método para obtener un Vector Aleatorio
+	 *
 	 * @return
 	 */
-	
-	
-	private Cluster getClusterMasCercano(Cluster pCluster) {
+	public Instancia getVectorAleatorio() {
 
-		Iterator<Cluster> it = resultado.iterator();
-		double min = Integer.MAX_VALUE;
-		double dis = 0;
-		Cluster resultado = null;
-
-		while (it.hasNext()) {
-			Cluster x = it.next();
-			if (!x.equals(pCluster)) {
-				dis = pCluster.getVector().getDistanceTo(x.getVector().getLista(), tipoDistancia);
-				if (dis < min) {
-					resultado = x;
-					min = dis;
-				}
-			}
-		}
-
-		return resultado;
+		return DataBase.getDataBase().getRandomVector();
 	}
-	
-	
-	
-	
-	
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////               MÉTODOS                          /////////////////////////////////////////////////
-///////////////////////////////              PARA CALCULAR                     /////////////////////////////////////////////////
-///////////////////////////////              LAS VARIACIONES                   /////////////////////////////////////////////////
-///////////////////////////////              DEL K-MEANS                       //////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	
+
 	/**
 	 * Recalcula los Clusters en la variación 2K del K-Means
+	 *
 	 * @param pClusters
 	 * @return
 	 */
 	public ArrayList<Cluster> recalcular2KClusters(ArrayList<Cluster> pClusters) {
-		
+
 		int filycol = pClusters.size();
 		float[][] matriz = new float[filycol][filycol];
 		int i = 0;
@@ -366,13 +323,13 @@ public class KmeansAlgorithm {
 		ArrayList<Cluster> Clusters = new ArrayList<Cluster>();
 		Iterator<Cluster> it = pClusters.iterator();
 		Iterator<Cluster> it2 = pClusters.iterator();
-		
+
 		while (it.hasNext()) {
-			
+
 			Cluster c = it.next();
-			
+
 			while (it2.hasNext()) {
-				
+
 				// if(matriz[i][j]!=null)
 				float dis = (float) c.getVector().getDistanceTo(it2.next().getVector().getLista(), tipoDistancia);
 				matriz[i][j] = dis;
@@ -381,15 +338,15 @@ public class KmeansAlgorithm {
 			}
 			i = i + 1;
 		}
-		
+
 		i = j = 0;
 		int idef = 0;
 		int jdef = 0;
 		float disdef = 0;
-		
+
 		while (i <= filycol) {
 			while (j <= filycol) {
-				
+
 				if (matriz[i][j] > disdef) {
 					idef = i;
 					jdef = j;
@@ -399,31 +356,12 @@ public class KmeansAlgorithm {
 			}
 			i++;
 		}
-		
+
 		Clusters.add(pClusters.get(idef));
 		Clusters.add(pClusters.get(jdef));
 		return Clusters;
 	}
-	
-	
-	/**
-	 * Asigna vectores iniciales en la variación 2k del K-Means
-	 */
-	public void asignarVector2kClusters() {
-		// TODO Auto-generated method stub
-		int i = 0;
-		System.out.println("Valores de los centroides iniciales: ");
-		while (i < 2 * k) {
-			Cluster nuevo = new Cluster(getVectorAleatorio());
-			if (!resultado.contains(nuevo)) {
-				resultado.add(nuevo);
-				nuevo.printCentroide();
-				i++;
-			}
-		}
-	}
-	
-	
+
 	/**
 	 * Asigna vectores en la variación de División del K-Means
 	 */
@@ -444,18 +382,46 @@ public class KmeansAlgorithm {
 		}
 	}
 
-	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////// MÉTODOS
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// /////////////////////////////////////////////////
+	/////////////////////////////// PARA CALCULAR
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// /////////////////////////////////////////////////
+	/////////////////////////////// LAS VARIACIONES
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// /////////////////////////////////////////////////
+	/////////////////////////////// DEL K-MEANS
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// //////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	/**
-	 * Devuelve un vector aleatorio para la variación de División del K-Means
-	 * @param k2
-	 * @param i
+	 * Halla el clúster más cercano a pCluster
+	 *
+	 * @param pCluster
 	 * @return
 	 */
-	private Instancia getVectorAleatorioDivision(int k2, int i) {
-		// TODO Auto-generated method stub
-		return DataBase.getDataBase().getRandomVectorDivision(k2, i);
+
+	private Cluster getClusterMasCercano(Cluster pCluster) {
+
+		Iterator<Cluster> it = resultado.iterator();
+		double min = Integer.MAX_VALUE;
+		double dis = 0;
+		Cluster resultado = null;
+
+		while (it.hasNext()) {
+			Cluster x = it.next();
+			if (!x.equals(pCluster)) {
+				dis = pCluster.getVector().getDistanceTo(x.getVector().getLista(), tipoDistancia);
+				if (dis < min) {
+					resultado = x;
+					min = dis;
+				}
+			}
+		}
+
+		return resultado;
 	}
-	
 
 	private Cluster getClusterMasDistante(Cluster pCluster) {
 		Iterator<Cluster> it = resultado.iterator();
@@ -482,9 +448,41 @@ public class KmeansAlgorithm {
 		return resultado;
 	}
 
-	
+	/**
+	 * Devuelve un vector aleatorio para la variación de División del K-Means
+	 *
+	 * @param k2
+	 * @param i
+	 * @return
+	 */
+	private Instancia getVectorAleatorioDivision(int k2, int i) {
+		// TODO Auto-generated method stub
+		return DataBase.getDataBase().getRandomVectorDivision(k2, i);
+	}
 
-	
-	
+	/**
+	 * Imprime los datos de los clusters por pantalla
+	 */
+	private void imprimirClusters() {
+
+		Iterator<Cluster> it = resultado.iterator();
+		Cluster act;
+		while (it.hasNext()) {
+			act = it.next();
+			act.printCentroide();
+			act.printCluster();
+		}
+	}
+
+	/**
+	 * Recalcula los centroides para la siguiente iteración.
+	 */
+	private void recalcularCentroides() {
+		Iterator<Cluster> it = resultado.iterator();
+		while (it.hasNext()) {
+			it.next().recalcularCentroide();
+		}
+
+	}
 
 }
